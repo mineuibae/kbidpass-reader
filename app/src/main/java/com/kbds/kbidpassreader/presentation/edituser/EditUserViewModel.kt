5ccf1c58053336c2f63d0b5a7565f5f430e7d4e4
mineui <alsdmlek4@gmail.com>
2020-12-04 18:08:23 +0900

    사용자 수정쪽 화면 개발중

--- app/src/main/java/com/kbds/kbidpassreader/presentation/edituser/EditUserViewModel.kt
+++ app/src/main/java/com/kbds/kbidpassreader/presentation/edituser/EditUserViewModel.kt
@@ -1,8 +1,39 @@
 package com.kbds.kbidpassreader.presentation.edituser
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kbds.kbidpassreader.data.Response
+import com.kbds.kbidpassreader.domain.usecase.audit.AddAuditUseCase
+import com.kbds.kbidpassreader.domain.usecase.user.GetUserUseCase
+import com.kbds.kbidpassreader.domain.usecase.user.UpdateUserUseCase
+import kotlinx.coroutines.launch
 
-class EditUserViewModel @ViewModelInject constructor(): ViewModel() {
+class EditUserViewModel @ViewModelInject constructor(
+    private val getUserUseCase: GetUserUseCase,
+    private val updateUserUseCase: UpdateUserUseCase,
+    private val addAuditUseCase: AddAuditUseCase
+): ViewModel() {
 
+    val userId = MutableLiveData<String>()
+    val userName = MutableLiveData<String>()
+    val userPassword = MutableLiveData<String>()
+    val userDeviceId = MutableLiveData<String>()
+    val userKBPass = MutableLiveData<String>()
+
+    fun start(id: String) {
+        viewModelScope.launch {
+            getUserUseCase(id).let { user ->
+                if(user is Response.Success) {
+                    userId.value = user.data.id
+                    userName.value = user.data.name
+                }
+            }
+        }
+    }
+
+    fun updateUser() {
+
+    }
 }
